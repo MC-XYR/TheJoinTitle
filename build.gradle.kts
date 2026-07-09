22b931c675f8ba3ab6d057bf64a2ea6d74352a92
MC_XYR <ds3ded@163.com>
2026-07-09 11:49:40 +0800

    v1.3.1；修复了2个bug;添加了papi支持

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,15 @@ plugins {
 }
 
 repositories {
-    mavenCentral()
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
+
 
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(21)
--- src/main/java/join/tittle/mcxyr.java
+++ src/main/java/join/tittle/mcxyr.java
@@ -1,6 +1,7 @@
 package join.tittle;
 
 import java.io.File;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
@@ -54,13 +55,18 @@ public final class mcxyr extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         String playerName = event.getPlayer().getName();
+        boolean papi = Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI");
 
         if (config.getBoolean("specific.enabled")) {
             List<String> specificList = config.getStringList("specific.players");
             for (String entry : specificList) {
                 String[] split = entry.split(":", 2);
                 if (split[0].equalsIgnoreCase(playerName)) {
-                    event.setJoinMessage(split[1].replace('&', '§'));
+                    String msg = split[1].replace('&', '§').replace("%player%", playerName);
+                    if (papi) {
+                        msg = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(event.getPlayer(), msg);
+                    }
+                    event.setJoinMessage(msg);
                     return;
                 }
             }
@@ -70,14 +76,22 @@ public final class mcxyr extends JavaPlugin implements Listener {
             List<String> specificallList = config.getStringList("specificall.players");
             for (String name : specificallList) {
                 if (name.equalsIgnoreCase(playerName)) {
-                    event.setJoinMessage(config.getString("specificall.message").replace('&', '§'));
+                    String msg = config.getString("specificall.message").replace('&', '§').replace("%player%", playerName);
+                    if (papi) {
+                        msg = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(event.getPlayer(), msg);
+                    }
+                    event.setJoinMessage(msg);
                     return;
                 }
             }
         }
 
         if (config.getBoolean("all.enabled")) {
-            event.setJoinMessage(config.getString("all.message").replace('&', '§'));
+            String msg = config.getString("all.message").replace('&', '§').replace("%player%", playerName);
+            if (papi) {
+                msg = me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(event.getPlayer(), msg);
+            }
+            event.setJoinMessage(msg);
         }
     }
 }
